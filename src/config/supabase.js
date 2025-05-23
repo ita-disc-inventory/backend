a8546246b3bcb386c12cf16047a1a88661099e4f
jskpaul <paulkim2025@u.northwestern.edu>
2025-05-22 22:01:52 -0500

    backend rls (add auth token to every call

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -3,6 +3,20 @@ require('dotenv').config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
+function getSupabaseClientWithToken(access_token) {
+  return createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    },
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+      detectSessionInUrl: false,
+    },
+  });
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
@@ -12,4 +26,4 @@ const supabase = createClient(supabaseUrl, supabaseKey, {
   },
 });
 
-module.exports = supabase;
+module.exports = { supabase, getSupabaseClientWithToken };
--- src/controllers/generalController.js
+++ src/controllers/generalController.js
@@ -1,18 +1,21 @@
-const supabase = require("../config/supabase");
+const { supabase, getSupabaseClientWithToken } = require("../config/supabase");
 const { getAllUsers } = require("./authController");
 
 // const { v4: uuidv4 } = require('uuid');
 const generalController = {
   async getOrders(req, res) {
     try {
+      const access_token =
+        req.cookies.session || req.headers.authorization?.split(" ")[1];
+      const supabaseWithToken = getSupabaseClientWithToken(access_token);
       // get all orders, join with users, programs, items
-      const { data, error } = await supabase
+      const { data, error } = await supabaseWithToken
         .from("orders")
         .select(
           "*, users(firstname,lastname, specialization), programs(program_title), items(*)",
         )
         .order("request_date", { ascending: false })
-        .neq("status", "cancelled"); 
+        .neq("status", "cancelled");
       if (error) {
         console.error("Error getting items:", error);
         return res.status(400).json({ error: error.message });
@@ -23,8 +26,13 @@ const generalController = {
     }
   },
   async getBudget(req, res) {
+    const access_token =
+      req.cookies.session || req.headers.authorization?.split(" ")[1];
+    const supabaseWithToken = getSupabaseClientWithToken(access_token);
     try {
-      const { data, error } = await supabase.from("programs").select("*");
+      const { data, error } = await supabaseWithToken
+        .from("programs")
+        .select("*");
       if (error) {
         console.error("Error getting programs:", error);
         return res.status(400).json({ error: error.message });
